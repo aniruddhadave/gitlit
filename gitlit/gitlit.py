237a0afa6a4698baba6f77f73566d9dec7a77863
Aniruddha Dave <aniruddhadave@gmail.com>
2018-07-15 02:02:34 +0530

    Add hashing objects

--- gitlit/gitlit.py
+++ gitlit/gitlit.py
@@ -6,6 +6,10 @@ from gitlit.utils import *
 import enum
 import os
 import sys
+from gevent.libev.corecext import self
+import hashlib
+import path
+from zipfile import zlib
 
 class GitLit(enum.Enum):
     commit = 1
@@ -14,6 +18,7 @@ class GitLit(enum.Enum):
 
     def init(repo):
         """Create directory for repo and initialize .git directory."""
+        from distutils.file_util import write_file
         if os.path.isdir(repo):
             """
             @todo: raise exception
@@ -30,4 +35,27 @@ class GitLit(enum.Enum):
             os.mkdir(os.path.join(repo, '.git', name))
         write_file(os.path.join(repo, '.git', 'HEAD'), b'ref: refs/heads/master')
         print('Initialized empty repository: {}'.format(repo))
+        
+    
+    def hash_object(self, data, obj_type, write=True):
+        """
+        Computes hash of object data of a given type.
+        If 'write' is True writes to object store.
+        Returns
+        -------
+        SHA-1 object as a hash string  
+        """
+        from distutils.file_util import write_file
+        header = '{} {}'.format(obj_type, len(data)).encode()
+        full_data = header + b'x\00' + data
+        sha1 = hashlib.sha1(full_data).hexdigest()
+        if write:
+            path = os.path.join('.git', 'objects', sha1[:2], sha1[2:])
+            if not os.path.exists(path):
+                os.makedirs(os.path.dirname(path), exist_ok= True)
+                write_file(path, zlib.compress(full_data))
+        return sha1
+    
+                
+                
         
--- gitlit/utils.py
+++ gitlit/utils.py
@@ -3,6 +3,7 @@ Utilities
 
 @author: Aniruddha Dave
 """
+import zlib
 
 def read_file(path):
     """Reads contents of file as bytes."""
@@ -13,3 +14,42 @@ def write_file(path, data):
     """Writes data bytes to file at given path."""
     with open(path, 'wb') as f:
         f.write(data)
+        
+def find_object(sha1_prefix):
+    """
+    Finds object with the given SHA-1 prefix.
+    Returns the path to object in object store
+    Raises a ValueError if there are no objects or multiple objects
+    """
+    if (len(sha1_prefix) < 2):
+        raise ValueError("Hash prefix must have 2 or more characters")
+    
+    obj_dir = os.path.join('.git', 'objects', sha1_prefix[:2])
+    rmng = sha1_prefix[2:]
+    objects = [name for name in os.listdir(obj_dir) if name.startswith(rmng)]
+    if not objects:
+        raise ValueError("Object {} not found".format(sha1_prefix))
+    if len(objects) >= 2:
+        raise ValueError("Multiple objects ({}) with prefix {}".format(
+            len(objects), sha1_prefix))
+    return os.path.join(obj_dir, objects[0])
+
+def read_object(sha1_prefix):
+    """
+    Reads object with given SHA-1 prefix
+    Return a tuple(object_type , data_bytes)
+    Raises ValueError of object not found
+    """
+    from builtins import int
+    path = find_object(sha1_prefix)
+    data_full = zlib.decompress(read_file(path))
+    idx = data_full.index(b'\x00')
+    header = data_full[:idx] 
+    object_type, size_data = header.decode().split()
+    size = int(size_data)
+    data = data_full[idx + 1:]
+    size_recvd = len(data)
+    assert size == len(data), 'expected size {} but received {} bytes'.format(
+        size, size_recvd)
+    return (object_type, data)
+        
